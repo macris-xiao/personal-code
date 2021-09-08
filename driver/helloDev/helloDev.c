c1004b5d50726e410913fcdd16262bf8a2044c88
Yu Xiao <yu.xiao@corigin.com>
2021-09-08 08:20:59 +0200

    Makefile: crate Makefile
    Completed the edit of Makefile
    
    Signed-off-by: Yu Xiao <yu.xiao@corigine.com>

--- driver/helloDev/helloDev.c
+++ driver/helloDev/helloDev.c
@@ -24,14 +24,14 @@ int flag = 0;
 int hello_open(struct inode *p, struct file *f)
 {
 	printk(KERN_EMERG"hello_open\r\n");
-	
+
 	return 0;
 }
 
 ssize_t hello_write(struct file*f, const char __user *u, size_t s, loff_t *l)
 {
 	printk(KERN_EMERG"hello_write\r\n");
-	
+
 	return 0;
 }
 
@@ -47,26 +47,39 @@ int hello_init(void)
 	/* Create dev number by MKDEV */
 	devNum = MKDEV(reg_major, reg_minor);
 
+	/* register dev number in kernel */
 	if(OK == register_chrdev_region(devNum, subDevNum, "hello world")){
 		printk(KERN_EMERG"register_chrdev_region ok \n");
 	}else {
 		printk(KERN_EMERG"register_chrdev_region error n");
-		
+
 		return ERROR;
 	}
 
 	printk(KERN_EMERG"hello driver init \n");
+	/* cdev represent a character device */
 	gDev = kzalloc(sizeof(struct cdev), GFP_KERNEL);
+	/* file_operations include some functions about how to operate file */
 	gFile = kzalloc(sizeof(struct file_operations), GFP_KERNEL);
-	gDeb = kzalloc(sizeof(struct cdev), GFP_KERNEL);
+	/* open read and write are function point, assigned values for them */
 	gFile->open = hello_open;
 	gFile->read = hello_read;
 	gFile->write = hello_write;
 	gFile->owner = THIS_MODULE;
+	/* build contact between gDev and gFile */
 	cdev_init(gDev, gFile);
-	cdev_add(gDev, devNum, 3);
+	/* build contact between gDev and devNum */
+	cdev_add(gDev, devNum, 1);
 
 	return 0;
+}
+
+void __exit hello_exit(void)
+{
+	cdev_del(gDev);
+	unregister_chrdev_region(devNum, subDevNum);
+
+	return;
 }
 
 /* the entry function of driver */
